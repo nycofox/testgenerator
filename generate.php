feb7903e58546eaa613a64ffe4451b52b4a0d716
nycofox <github@alkis.net>
2014-10-08 10:10:14 +0200

    Fixed formatting

--- generate.php
+++ generate.php
@@ -4,7 +4,7 @@ require_once('vendor/fzaninotto/faker/src/autoload.php');
 $faker = Faker\Factory::create();
 
 // Load template
-$xt=simplexml_load_file('template.xml');
+$xt = simplexml_load_file('template.xml');
 
 // Start a new XML file
 $xml = new XMLWriter();
@@ -15,10 +15,6 @@ generateElement($xt);
 $xml->endDocument();
 $xml->flush();
 
-/**
- * @param $input
- * @return string
- */
 function getValue($input)
 {
     global $faker;
@@ -36,25 +32,24 @@ function generateElement($current)
 
     // Include the requested number of times
     $currentcount = $current->attributes()->inc ? $current->attributes()->inc : 1;
-	
-	for ($x=1; $x<=$currentcount; $x++) {
-		$xml->startElement($current->getName());
-		foreach($current->attributes() as $attribute => $value)
-		{
-			if(!($attribute === 'inc')) {
-				$xml->startAttribute($attribute);
-				$xml->text(getValue($value));
-				$xml->endAttribute();
-			}
-		}
-	
-		if($current->count() > 0 ) {
-			foreach ($current->children() as $child) {
-				generateElement($child);
-			}
-		} else {
-			$xml->text(getValue($current));
-		}
-		$xml->endElement();
-	}
+
+    for ($x = 1; $x <= $currentcount; $x++) {
+        $xml->startElement($current->getName());
+        foreach ($current->attributes() as $attribute => $value) {
+            if (!($attribute === 'inc')) {
+                $xml->startAttribute($attribute);
+                $xml->text(getValue($value));
+                $xml->endAttribute();
+            }
+        }
+
+        if ($current->count() > 0) {
+            foreach ($current->children() as $child) {
+                generateElement($child);
+            }
+        } else {
+            $xml->text(getValue($current));
+        }
+        $xml->endElement();
+    }
 }
